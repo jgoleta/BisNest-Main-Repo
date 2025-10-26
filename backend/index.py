e3a4075783252d48ff4e739478013176299b2626
jgoleta <jgoleta@gbox.adnu.edu.ph>
2025-10-26 23:41:20 +0800

    Added Customer Edit tae

--- backend/index.py
+++ backend/index.py
@@ -151,14 +151,25 @@ def signupPage(request):
 
 def customerInfoPage(request):
     if request.method == 'POST':
-        form = CustomerForm(request.POST)
-        if form.is_valid():
-            form.save()
+        edit_id = request.POST.get('edit_id')
+
+        if edit_id:  # existing customer edit
+            customer = get_object_or_404(Customer, pk=edit_id)
+            # mga pdeng iedit
+            customer.name = request.POST.get('name', customer.name)
+            customer.phone = request.POST.get('phone', customer.phone)
+            customer.address = request.POST.get('address', customer.address)
+            customer.save()
             return redirect('customer')
-    else:
-        form = CustomerForm()
+
+        else:  # create
+            form = CustomerForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('customer')
 
     customers = Customer.objects.all()
+    form = CustomerForm()
     return render(request, 'customer.html', {
         'form': form,
         'customers': customers
