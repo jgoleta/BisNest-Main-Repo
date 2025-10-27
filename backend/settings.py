30699eae7db791772f5b95f0d9c6e189069b9e2c
Mcleilan Jay Cosa <mjcosa@gbox.adnu.edu.ph>
2025-10-27 18:23:23 +0800

    Turned DEBUG = FALSE

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w+pn$_7ymqew+^_%c23jeljx4rinl29v!qj-b%)xqa)o7gk^&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
